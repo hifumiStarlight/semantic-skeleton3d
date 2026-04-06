0fd1505a7dcc9322798de2a73f53384b1051ac71
Timur Sinnurow <runelsij@gmail.com>
2026-04-06 21:42:04 +0200

    feat: add initial python classifier stage

---
+++ python/infer.py
@@ -0,0 +1,22 @@
+import numpy as np
+import open3d as o3d
+
+ROOM = "../Pointcept/data/s3dis/Area_5/office_39" # path to room with data set
+TARGET_LABEL = 3  # beam
+
+coord  = np.load(f"{ROOM}/coord.npy")    # (N, 3)
+color  = np.load(f"{ROOM}/color.npy")    # (N, 3) uint8
+seg    = np.load(f"{ROOM}/segment.npy").flatten()  # (N,)
+
+# Build color map: beam=red, rest=dark grey
+colors = np.full((len(coord), 3), 0.2)   # grey background
+colors[seg == TARGET_LABEL] = [1.0, 0.1, 0.1]  # red beams
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(coord.astype(np.float64))
+pcd.colors = o3d.utility.Vector3dVector(colors)
+
+o3d.io.write_point_cloud("../assets/labeled.ply", pcd) 
+
+beam_count = (seg == TARGET_LABEL).sum()
+print(f"Exported {len(coord)} points ({beam_count} beams) to assets/labeled.ply")
